5b997cf403eed51d8a8e003fac37f265e6528654
yingming25 <karcnonf@gmail.com>
2020-07-14 18:21:15 +0800

    Update 2020-07-14T10:19:43.028Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,28 +1,26 @@
-$(document).ready(() => {
+"use strict";
+
+$(document).ready(function () {
   $('[data-toggle="offcanvas"]').on('click', function (e) {
     e.preventDefault();
     $('.offcanvas-collapse').toggleClass('open');
   });
-
   $('.goBack').on('click', function (e) {
     e.preventDefault();
     $('.offcanvas-collapse').removeClass('open');
   });
-
   $('.menuItemExpand').on('click', function (e) {
     e.preventDefault();
     $('.menuItemSubList').slideToggle(500);
     $('.menuItemSubList').toggleClass('expand');
     $(this).toggleClass('active');
   });
-
-  $('.orderSummaryIcon').on('click', function(e) {
+  $('.orderSummaryIcon').on('click', function (e) {
     e.preventDefault();
     $('.orderSummaryDetail').slideToggle(600);
     $(this).toggleClass('active');
   });
-
-  $('.contactIcon').on('click', function(e) {
+  $('.contactIcon').on('click', function (e) {
     e.preventDefault();
     $('.contactDetail').slideToggle(600);
     $('.contactDetail').toggleClass('d-flex');
@@ -30,8 +28,7 @@ $(document).ready(() => {
     $(this).parent().toggleClass('ml-auto');
     $(this).toggleClass('active');
   });
-
-  $('.shippingIcon').on('click', function(e) {
+  $('.shippingIcon').on('click', function (e) {
     e.preventDefault();
     $('.shippingDetail').slideToggle(600);
     $('.shippingDetail').toggleClass('d-flex');
@@ -39,7 +36,6 @@ $(document).ready(() => {
     $(this).parent().toggleClass('ml-auto');
     $(this).toggleClass('active');
   });
-
   var swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     spaceBetween: 16,
@@ -47,17 +43,17 @@ $(document).ready(() => {
     breakpoints: {
       640: {
         slidesPerView: 2,
-        spaceBetween: 16,
+        spaceBetween: 16
       },
       768: {
         slidesPerView: 3,
-        spaceBetween: 16,
+        spaceBetween: 16
       },
       1024: {
         slidesPerView: 3,
-        spaceBetween: 30,
-      },
+        spaceBetween: 30
+      }
     }
   });
 });
-
+//# sourceMappingURL=all.js.map
